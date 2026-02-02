f961acd0d50a4487009921b81bcfb67ab7f3a13e
Amir Bashiri <armin69me@gmail.com>
2026-02-02 16:41:21 +0200

    Delete case1_1D directory

--- case1_1D/run.sh
+++
@@ -1,8 +0,0 @@
-blockMesh
-topoSet
-splitMeshRegions -cellZones -overwrite
-paraFoam -touchAll
-decomposePar -allRegions -force
-mpirun -np 8 chtMultiRegionFoam -parallel > chtMultiRegionFoam.log 
-reconstructPar -allRegions -latestTime
-
